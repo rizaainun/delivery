6be8bae2e5dfa3eac681d5c2e8aa8f147d547f8a
Anitaputri <108598332+anitaputrirmdhn@users.noreply.github.com>
2023-01-05 21:51:44 +0700

    membetulkan yang error

--- app/src/main/java/com/riza/delivery/Activity/IntroActivity.java
+++ app/src/main/java/com/riza/delivery/Activity/IntroActivity.java
@@ -1,4 +1,4 @@
-package com.riza.delivery.Activity;
+package com.anita.delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -7,20 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.riza.delivery.R;
-
 public class IntroActivity extends AppCompatActivity {
-private ConstraintLayout startBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        startBtn=findViewById(R.id.startBtn);
+        ConstraintLayout startBtn=findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                startActivity(new Intent(IntroActivity.this, IntroActivity.class));
             }
         });
     }
