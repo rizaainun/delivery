807782f52faf033f26b0c94368b389f28e1f7ff4
anitaputri <anitaaramadhani@gmail.com>
2023-01-10 14:01:38 +0700

    edit IntroActivity

--- app/src/main/java/com/riza/delivery/Activity/IntroActivity.java
+++ app/src/main/java/com/riza/delivery/Activity/IntroActivity.java
@@ -12,9 +12,9 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.id.layout.activity_intro);
 
-        ConstraintLayout startBtn=findViewById(R.id.startBtn);
+        ConstraintLayout startBtn = findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
