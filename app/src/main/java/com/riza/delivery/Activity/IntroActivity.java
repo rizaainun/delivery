6cd564e9fb3b6fc58d25f116834c725221d018dd
rizaainun <rizaainunj@gmail.com>
2023-01-13 16:29:02 +0700

    memperbaiki yang eror

--- app/src/main/java/com/riza/delivery/Activity/IntroActivity.java
+++ app/src/main/java/com/riza/delivery/Activity/IntroActivity.java
@@ -1,27 +1,28 @@
 package com.riza.delivery.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.riza.delivery.R;
 
 public class IntroActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.id.layout.activity_introo);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_intro );
 
-        ConstraintLayout startBtn = findViewById(R.id.startBtn);
-        startBtn.setOnClickListener(new View.OnClickListener() {
+        ConstraintLayout startBtn = findViewById( R.id.startBtn );
+        startBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(IntroActivity.this, IntroActivity.class));
+                startActivity( new Intent(IntroActivity.this,MainActivity.class) );
             }
-        });
+        } );
     }
 }
