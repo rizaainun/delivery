660b349686d7475b5518f10d2bb4439b0da2f6a3
rizaainun <rizaainunj@gmail.com>
2023-01-18 13:38:38 +0700

    menambah show detail

---
+++ app/src/main/java/com/riza/delivery/Activity/ShowDetailActivity.java
@@ -0,0 +1,16 @@
+package com.riza.delivery.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.riza.delivery.R;
+
+public class ShowDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_show_detail );
+    }
+}
