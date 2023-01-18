68b361b78607fd01470d0d3a5ea3ce336bbe4d8b
anitaputri <anitaaramadhani@gmail.com>
2023-01-18 15:45:53 +0700

    menambahkan detail

--- app/src/main/java/com/riza/delivery/Activity/ShowDetailActivity.java
+++ app/src/main/java/com/riza/delivery/Activity/ShowDetailActivity.java
@@ -1,16 +1,34 @@
 package com.riza.delivery.Activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.riza.delivery.R;
+import com.riza.delivery.data.PopularEntity;
 
 public class ShowDetailActivity extends AppCompatActivity {
+    TextView tvName,tvfee;
+    ImageView imgpopular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_show_detail );
+
+        tvName = findViewById(R.id.textView4);
+        imgpopular = findViewById(R.id.foodPic);
+        tvfee = findViewById(R.id.textView6);
+
+        PopularEntity popular = getIntent().getParcelableExtra("POPLAR");
+        tvName.setText(popular.getName());
+        tvfee.setText(popular.getFee());
+        Glide.with(ShowDetailActivity.this)
+                .load(popular.getImage())
+                .into(imgpopular);
+
     }
 }
--- app/src/main/java/com/riza/delivery/Adapter/PopularAdapter.java
+++ app/src/main/java/com/riza/delivery/Adapter/PopularAdapter.java
@@ -1,5 +1,6 @@
 package com.riza.delivery.Adapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,57 +10,61 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.riza.delivery.Activity.ShowDetailActivity;
 import com.riza.delivery.Domain.CategoryDomain;
 import com.riza.delivery.Domain.FoodDomain;
 import com.riza.delivery.R;
+import com.riza.delivery.data.PopularEntity;
 
 import java.util.ArrayList;
 
-public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.ViewHolder> {
-    ArrayList<FoodDomain> categoryDomains;
-    private ArrayList<CategoryDomain> PopularDomains;
+public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.ListViewHolder> {
+    private ArrayList<PopularEntity> listPopular;
 
-    public PopularAdapter(ArrayList<FoodDomain> CtegoryList) {
-    }
-
-    public void setCategoryDomains(ArrayList<CategoryDomain> categoryDomains) {
-        this.PopularDomains = categoryDomains;
-    }
+    public PopularAdapter(ArrayList<PopularEntity>list) { this.listPopular = list; }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View inflate = LayoutInflater.from(parent.getContext()).inflate( R.layout.viewholder_category,parent,false);
-
-        return new PopularAdapter.ViewHolder(inflate);
+    public PopularAdapter.ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_popular,parent,false);
+        return new ListViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PopularAdapter.ViewHolder holder,int position) {
-        holder.title.setText(PopularDomains.get(position).getTitle());
-        holder.fee.setText(String.valueOf(PopularDomains.get(position).getFee));
+    public void onBindViewHolder(@NonNull PopularAdapter.ListViewHolder holder, int position) {
+        final PopularEntity popularEntity = listPopular.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(popularEntity.getImage())
+                .into(holder.imgPopuler);
+        holder.tvName.setText(popularEntity.getName());
+        holder.tvfee.setText(popularEntity.getFee());
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        int drawableResourceId=holder.itemView.getContext().getResources()
-                .getIdentifier(PopularDomains.get(position).getPic(),"drawable",
-                        holder.itemView.getContext().getPackageName());
+                Intent intent = new Intent(holder.itemView.getContext(), ShowDetailActivity.class);
+                intent.putExtra("POPULAR",popularEntity);
+                holder.itemView.getContext().startActivity(intent);
+            }
+
+        });
     }
 
     @Override
-    public int getItemCount() { return categoryDomains.size(); }
+    public int getItemCount() {
+        return 0;
+    }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView title, fee;
-        ImageView pic;
-        ImageView addBtn;
-
-        public ViewHolder(@NonNull View itemView) {
+     class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPopuler;
+        TextView tvName,tvfee;
+        public ListViewHolder(@NonNull View itemView) {
             super(itemView);
-            title = itemView.findViewById(R.id.title);
-            pic = itemView.findViewById(R.id.pic);
-            fee = itemView.findViewById(R.id.fee);
-            addBtn = itemView.findViewById(R.id.addBtn);
+            imgPopuler = itemView.findViewById(R.id.pic);
+            tvName = itemView.findViewById(R.id.title);
+            tvfee = itemView.findViewById(R.id.fee);
         }
     }
 }
-
