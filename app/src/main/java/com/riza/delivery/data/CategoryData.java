a5e4e5cc6de95508df99b45b514ef48199dd5931
rizaainun <rizaainunj@gmail.com>
2023-01-18 15:00:20 +0700

    menambah entity dan data

--- app/src/main/java/com/riza/delivery/data/CategoryData.java
+++ app/src/main/java/com/riza/delivery/data/CategoryData.java
@@ -2,6 +2,8 @@ package com.riza.delivery.data;
 
 import com.riza.delivery.R;
 
+import java.util.ArrayList;
+
 public class CategoryData {
     private static String[] categoryName = {
             "Drink",
@@ -18,4 +20,15 @@ public class CategoryData {
             R.drawable.cat_4,
             R.drawable.cat_5,
     };
+
+    public static ArrayList<CategoryEntity>getListCategory(){
+        ArrayList<CategoryEntity>list = new ArrayList<>();
+        for (int position = 0; position < categoryName.length; position++){
+            CategoryEntity categoryEntity = new CategoryEntity();
+            CategoryEntity.setName(categoryName[position]);
+            CategoryEntity.setImage(categoryName[position]);
+            list.add(categoryEntity);
+        }
+        return list;
+    }
 }
