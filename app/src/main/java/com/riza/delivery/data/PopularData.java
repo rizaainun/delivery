29668f9c4b9e8256996397b42d9409efdf95eece
anitaputri <anitaaramadhani@gmail.com>
2023-01-18 14:08:27 +0700

    mengisi dataclass Populer

--- app/src/main/java/com/riza/delivery/data/PopularData.java
+++ app/src/main/java/com/riza/delivery/data/PopularData.java
@@ -1,4 +1,23 @@
 package com.riza.delivery.data;
 
+import com.riza.delivery.R;
+
 public class PopularData {
+    private static String[] populerName = {
+            "Chicken and Cauliflower Rice Bowl",
+            "Crispy Baked Honey Garlic Chicken",
+            "Instant Pot Spaghetti and Meatballs",
+    };
+
+    private static int[] populerImage = {
+            R.drawable.food_1,
+            R.drawable.food_2,
+            R.drawable.food_3
+    };
+
+    private static String[] populerfee = {
+            "$2.00",
+            "$5.00",
+            "$1.00",
+    };
 }
