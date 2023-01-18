e6adaa3fd044a0aacff710a9f7de1b10a825c9a5
rizaainun <rizaainunj@gmail.com>
2023-01-18 14:07:39 +0700

    mengisi data class category

--- app/src/main/java/com/riza/delivery/Activity/MainActivity.java
+++ app/src/main/java/com/riza/delivery/Activity/MainActivity.java
@@ -1,17 +1,10 @@
 package com.riza.delivery.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 
-import com.riza.delivery.Adaptor.CategoryAdaptor;
-import com.riza.delivery.Domain.CategoryDomain;
-import com.riza.delivery.Domain.FoodDomain;
-import com.riza.delivery.R;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import com.riza.delivery.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,32 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void recyclerViewCategory() {
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
-        RecyclerView recyclerViewCategoryList = findViewById(R.id.recyclerView);
-        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
 
-        ArrayList<CategoryDomain> category=new ArrayList<>();
-        category.add(new CategoryDomain("Drink","R.drawable.cat_1"));
-        category.add(new CategoryDomain("Snack","R.drawable.cat_2"));
-        category.add(new CategoryDomain("FastFood","R.drawable.cat_3"));
-        category.add(new CategoryDomain("Dessert","R.drawable.cat_4"));
-        category.add(new CategoryDomain("Donut","R.drawable.cat_5"));
-
-        RecyclerView.Adapter<CategoryAdaptor.ViewHolder> adapter = new CategoryAdaptor(category);
-           recyclerViewCategoryList.setAdapter(adapter);
     }
     private void recyclerViewPopular() {
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        RecyclerView recyclerViewPopularList = findViewById(R.id.recyclerView2);
-        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
 
-        ArrayList<FoodDomain> foodList = new ArrayList<>();
-        foodList.add(new FoodDomain("R.drawable.food_1","Chicken and Cauliflower"));
-        foodList.add(new FoodDomain("R.drawable.food_2","Crispy Baked Honey"));
-        foodList.add(new FoodDomain("R.drawable.food_3","Instant Pot Spaghetti"));
-
-        RecyclerView.Adapter<PopularAdaptor.ViewHolder> adapter2 = new PopularAdaptor(foodList);
-        recyclerViewPopularList.setAdapter(adapter2);
     }
 }
 
--- app/src/main/java/com/riza/delivery/data/CategoryData.java
+++ app/src/main/java/com/riza/delivery/data/CategoryData.java
@@ -1,4 +1,21 @@
 package com.riza.delivery.data;
 
+import com.riza.delivery.R;
+
 public class CategoryData {
+    private static String[] categoryName = {
+            "Drink",
+            "Snack",
+            "Fastfood",
+            "Dessert",
+            "Donut"
+    };
+
+    private static int[] categoryImage = {
+            R.drawable.cat_1,
+            R.drawable.cat_2,
+            R.drawable.cat_3,
+            R.drawable.cat_4,
+            R.drawable.cat_5,
+    };
 }
